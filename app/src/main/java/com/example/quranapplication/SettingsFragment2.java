71e146bffcaa52f905b7a43f544ee75d2707f034
saraabdalla238 <saraabdallahamd@gmail.com>
2021-04-02 14:47:08 +0200

    add setting fragment

---
+++ app/src/main/java/com/example/quranapplication/SettingsFragment2.java
@@ -0,0 +1,13 @@
+package com.example.quranapplication;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment2 extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    }
+}
